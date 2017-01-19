82d37fd4eaebe0fa46bde0962c03089aade28abe
stu <stu@ai221lab0339m04.aim.edmc.stu>
2017-01-19 13:56:55 -0600

    Added reverse-a-string.js

--- reverse-a-string.js
+++ reverse-a-string.js
@@ -1,4 +1,14 @@
-/*The split() method splits a String object into an array of strings by separating the string into substrings.*/
+/* reverse-a-string.js
+
+The split() method splits a String object into an array of strings by separating the string into substrings. 
+
+Once split, the array methods reverse() and join() can be used.
+
+The reverse() method reverses an array in place. The first array element becomes the last and the last becomes the first.
+
+The join() method joins all elements of an array into a string.
+
+*/
 
 function reverseString(str) {
   return str.split('').reverse().join('');
