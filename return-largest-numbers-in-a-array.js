8696a3e63754fc64667fdb0ec409f6c0735453f6
SCROLLERMAN <SCROLLERMAN@users.noreply.github.com>
2017-01-20 09:35:28 -0600

    Added find-the-longest-word-in-a-string.js

---
+++ return-largest-numbers-in-a-array.js
@@ -0,0 +1,29 @@
+/* find-the-longest-word-in-a-string.js
+
+Create a variable to hold the largest numbers of all for arrays.
+
+Create a FOR loop that will iterate through the arrays.
+
+Create a FOR loop that will iterate through the subarrays.
+
+Return the largest numbers of each sub-arrays
+
+*/
+
+function largestOfFour(arr) {
+    var largestNumber = [0, 0, 0, 0];
+    for (var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
+        for (var subArrayIndex = 0; subArrayIndex < arr[arrayIndex].length; subArrayIndex++) {
+            if (arr[arrayIndex][subArrayIndex] > largestNumber[arrayIndex]) {
+                largestNumber[arrayIndex] = arr[arrayIndex][subArrayIndex];
+            }
+        }
+    }
+    return largestNumber;
+}
+largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+]);
